98c1e159bc405586d31e7b644cb3c401d56613b5
Selion11 <gdecaro@itba.edu.ar>
2023-11-03 18:03:43 -0300

    Chase enemy

--- Gravity-Breakers/Assets/Scripts/AI/Enemy.cs
+++ Gravity-Breakers/Assets/Scripts/AI/Enemy.cs
@@ -1,7 +1,32 @@
-namespace AI
+
+using UnityEngine;
+
+public class FlyingEnemy : MonoBehaviour
 {
-    public class Enemy
-    {
-        
-    }
+        public float speed = 10f;
+        public GameObject player;
+        public bool chase = false;
+        public Transform startPoint;
+
+        private void Start()
+        {     
+                player = GameObject.FindGameObjectWithTag("Player");
+        }
+
+
+        void Update()
+        {
+
+                if (chase == true)
+                {
+                        Chase();
+                }
+        }
+
+        private void Chase()
+        {
+                Debug.Log("chase");
+                transform.position = 
+                        Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        }
 }
