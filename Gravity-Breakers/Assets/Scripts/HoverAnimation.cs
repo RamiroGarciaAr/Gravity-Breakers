15e3f8f60254342f6b3bdde94dfa23595008f5bb
Selion11 <gdecaro@itba.edu.ar>
2023-11-04 19:17:14 -0300

    Containers

--- Gravity-Breakers/Assets/Scripts/HoverAnimation.cs
+++ Gravity-Breakers/Assets/Scripts/HoverAnimation.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 public class HoverAnimation : MonoBehaviour
 {
 
-    public float amplitud = 9f;
-    public float speed = 1.3f;
+    public float amplitud = 2f;
+    public float speed = 1f;
+    public float range = 2f;
+
+    public LayerMask whatIsGround;
+
+    public float rayDistance;
     //public Transform hoverPoint;
     private void Update()
     {
+        RaycastHit hit;
         Vector3 p = transform.position;
-        p.y = amplitud * Mathf.Cos(Time.time * speed) + 0.9f;
+        if (Physics.Raycast(transform.position,Vector3.down,rayDistance,whatIsGround))
+        {
+            Debug.Log("Found");
+            p.y = (amplitud * Mathf.Pow(Mathf.Cos(Time.time * speed),2 )) + range;
+        }
+        else
+        {
+            p.y =( amplitud * Mathf.Cos(Time.time * speed) ) + range;
+        }
+        
         transform.position = p;
     }
 }
