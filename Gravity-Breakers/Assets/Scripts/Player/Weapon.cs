9f98043449d939f9440e4e806f3df10e874dcf60
RamiroGarciaAr <67532049+RamiroGarciaAr@users.noreply.github.com>
2023-11-04 20:53:08 -0300

    Recoil

--- Gravity-Breakers/Assets/Scripts/Player/Weapon.cs
+++ Gravity-Breakers/Assets/Scripts/Player/Weapon.cs
@@ -1,16 +1,25 @@
 using DG.Tweening;
 using UnityEngine;
+using DG.Tweening;
 
 public class Weapon : MonoBehaviour
 {
 
+    public Transform gunBarrel;
+    public float bulletSpeed;
     public float dmg = 10;
     public Camera fpsCam;
     
     public GameObject Bullet;
     public Animator anim;
-
+    
     public KeyCode reloadKey = KeyCode.R;
+    
+    [Header("Recoil")] 
+    [SerializeField] private float recoilX;
+    [SerializeField] private float recoilY;
+    [SerializeField] private float recoilZ;
+    
     // Update is called once per frame
     void Update()
     {
@@ -30,10 +39,12 @@ public class Weapon : MonoBehaviour
         if (Input.GetKeyUp(reloadKey))
             anim.SetBool("isReloading",false);
     }
+    
 
     private void Shoot()
     {
         RaycastHit hit;
+        RecoilFire();
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit))
         {
             if (hit.transform.gameObject.CompareTag("Enemy"))
@@ -42,11 +53,11 @@ public class Weapon : MonoBehaviour
                 hp.getHit(dmg);
             }
         }
+    
 
-
-        //Rigidbody rb = Instantiate(Bullet,gunBarrel.transform.position,Quaternion.identity).GetComponent<Rigidbody>(); 
-        //rb.AddForce(gunBarrel.transform.forward * bulletSpeed,ForceMode.Impulse);
-
-
+    }
+    void RecoilFire()
+    {
+        fpsCam.DOShakeRotation(0.1f,new Vector3(recoilX,Random.Range(-recoilY,recoilY),Random.Range(-recoilY,recoilY)),1,90f,false,ShakeRandomnessMode.Harmonic);
     }
 }
