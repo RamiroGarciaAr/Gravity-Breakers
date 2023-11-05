9c671d04dcf3ef9a3deb22b01e0070c79f566210
Selion11 <gdecaro@itba.edu.ar>
2023-11-05 09:04:40 -0300

    Home files for loading

--- Gravity-Breakers/Assets/Scripts/homeScreen.cs
+++ Gravity-Breakers/Assets/Scripts/homeScreen.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 public class homeScreen: MonoBehaviour
 {
     public void stage1Go()
     {
         Debug.Log("Go To Stage1");
+        SceneManager.LoadScene("ACA VA EL NOMBRE");
     }
 }
