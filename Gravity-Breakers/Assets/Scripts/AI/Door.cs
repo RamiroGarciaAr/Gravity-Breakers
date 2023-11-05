618acad6f2c2baaac5a501dcf9954c40b096d449
Selion11 <gdecaro@itba.edu.ar>
2023-11-05 05:13:36 -0300

    Lerping Door

--- Gravity-Breakers/Assets/Scripts/AI/Door.cs
+++ Gravity-Breakers/Assets/Scripts/AI/Door.cs
@@ -1,15 +1,32 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class Door : MonoBehaviour
 {
     public Button bot;
+    public Transform lerpTo;
 
     public void Update()
     {
         if (bot.open)
         {
-            Destroy(gameObject);
+            //Destroy(gameObject);
+            StartCoroutine(LerpPosition(lerpTo.position, 5));
         }
     }
+    
+    public Vector3 positionToMoveTo;
+        IEnumerator LerpPosition(Vector3 targetPosition, float duration)
+        {
+            float time = 0;
+            Vector3 startPosition = transform.position;
+            while (time < duration)
+            {
+                transform.position = Vector3.Lerp(startPosition, targetPosition, time / duration);
+                time += Time.deltaTime;
+                yield return null;
+            }
+            transform.position = targetPosition;
+        }
 }
