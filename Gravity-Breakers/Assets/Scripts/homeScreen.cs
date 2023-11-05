fd483e3ef204a1dedd0b339ae50f00c3f6bec383
Selion11 <gdecaro@itba.edu.ar>
2023-11-05 09:01:48 -0300

    Home Screen Done

--- Gravity-Breakers/Assets/Scripts/homeScreen.cs
+++ Gravity-Breakers/Assets/Scripts/homeScreen.cs
@@ -1,10 +1,6 @@
 using UnityEngine;
 public class homeScreen: MonoBehaviour
 {
-    public void tutorialGo()
-    {
-        Debug.Log("Go To Tutorial");
-    }
     public void stage1Go()
     {
         Debug.Log("Go To Stage1");
